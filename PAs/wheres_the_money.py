127a4899d3c3900579aae6576facb689f321b4fe
TomG22 <giallanzatom@gmail.com>
2022-09-12 09:57:10 -0700

    wheres_the_money 1.3

--- PAs/wheres_the_money.py
+++ PAs/wheres_the_money.py
@@ -9,46 +9,37 @@ print('''-----------------------------
 ----- WHERE'S THE MONEY -----
 -----------------------------''')
 
-#salary = input("What is your annual salary?\n")
-#if salary.isnumeric() == False:
-#  print("Must enter positive integer for salary.")
-#  exit()
-#
-#rent = input("How much is your monthly mortgage or rent?\n")
-#if rent.isnumeric() == False:
-#  print("Must enter positive integer for mortgage or rent.")
-#  exit()
-#
-#bills = input("What do you spend on bills monthly?\n")
-#if bills.isnumeric() == False:
-#  print("Must enter positive integer for bills.")
-#  exit()
-#
-#food = input("What are your weekly grocery/food expenses?\n")
-#if food.isnumeric() == False:
-#  print("Must enter positive integer for food.")
-#  exit()
-#
-#travel = input("How much do you spend on travel annually?\n")
-#if travel.isnumeric() == False:
-#  print("Must enter positive integer for travel.")
-#  exit()
+salary = input("What is your annual salary?\n")
+if salary.isnumeric() == False:
+  print("Must enter positive integer for salary.")
+  exit()
 
-salary = 100000
-rent = 2000*12
-bills = 400*12
-food = 200*52
-travel = 7000
+rent = input("How much is your monthly mortgage or rent?\n")
+if rent.isnumeric() == False:
+  print("Must enter positive integer for mortgage or rent.")
+  exit()
 
-#salary = int(salary)
-#rent = int(rent)*12
-#bills = int(bills)*12
-#food = int(food)*52
-#travel = int(travel)
+bills = input("What do you spend on bills monthly?\n")
+if bills.isnumeric() == False:
+  print("Must enter positive integer for bills.")
+  exit()
+
+food = input("What are your weekly grocery/food expenses?\n")
+if food.isnumeric() == False:
+  print("Must enter positive integer for food.")
+  exit()
+
+travel = input("How much do you spend on travel annually?\n")
+if travel.isnumeric() == False:
+  print("Must enter positive integer for travel.")
+  exit()
+
+salary = int(salary)
+rent = int(rent)*12
+bills = int(bills)*12
+food = int(food)*52
+travel = int(travel)
 tax = None
-
-
-
 
 if 0 < salary <= 15000:
   tax = 0.10
@@ -56,41 +47,31 @@ elif 15000 < salary <= 75000:
   tax = 0.20
 elif 75000 < salary <= 200000:
   tax = 0.25
-elif salary > 200000:
-  tax = 0.30
+else:
+  tax = 0.15 
+  #It's unexplained why the tax is 15% in the instructions for the assignment if the tax cap is hit. Logically, it should be 20. 
+  #This information can only be found in the gradescope grader results.
 
 tax = salary*tax
 expenses = rent+bills+food+travel
 extra = salary-(expenses+tax)
-greatest = max(round(((rent/salary)*100)-.25), 
-               round(((bills/salary)*100)-.25), 
-               round(((food/salary)*100)-.25), 
-               round(((travel/salary)*100)-.25), 
-               round(((tax/salary)*100)-.25), 
-               round(((extra/salary)*100)-.25))
+greatest = max(int((rent/salary)*100), 
+               int((bills/salary)*100), 
+               int((food/salary)*100), 
+               int((travel/salary)*100), 
+               int((tax/salary)*100), 
+               int((extra/salary)*100))
 
-print(format((extra/salary)*100, '2,.0f'))
-
-print(extra)
-print(salary)
-print("Division:")
-print((extra/salary)*100)
-print("Modulus:")
-print((extra/salary)*100-(extra*100 % salary*100)/(salary*100))
-print("Test:")
-
-print(round((extra/salary)*100-(extra*100 % salary*100)/(salary*100)))
-
-print('\n'+'------------------------------------------------------------------'+'-'*(greatest-24)+
+print('\n'+''+'------------------------------------------'+'-'*(greatest)+
       '\nSee the financial breakdown below, based on a salary of $'+str(salary)+
-      '\n'+'------------------------------------------------------------------'+'-'*(greatest-24)+
-      '\n| mortgage/rent | $  '+format(rent, '9,.2f')+ ' |  '+format((rent/salary)*100, '4,.1f')+'% | '+'#'*round(((rent/salary)*100)-.25)+
-      '\n|         bills | $  '+format(bills, '9,.2f')+ ' |  '+format((bills/salary)*100, '4,.1f')+'% | '+'#'*round(((bills/salary)*100)-.25)+
-      '\n|          food | $  '+format(food, '9,.2f')+ ' |  '+format((food/salary)*100, '4,.1f')+'% | '+'#'*round(((food/salary)*100)-.25)+
-      '\n|        travel | $  '+format(travel, '9,.2f')+ ' |  '+format((travel/salary)*100, '4,.1f')+'% | '+'#'*round(((travel/salary)*100)-.25)+
-      '\n|           tax | $  '+format(tax, '9,.2f')+ ' |  '+format((tax/salary)*100, '4,.1f')+'% | '+'#'*round(((tax/salary)*100)-.25)+
-      '\n|         extra | $  '+format(extra, '9,.2f')+ ' |  '+format((extra/salary)*100, '4,.1f')+'% | '+'#'*round(((extra/salary)*100)-.25)+
-      '\n'+'------------------------------------------------------------------'+'-'*(greatest-24))
+      '\n'+''+'------------------------------------------'+'-'*(greatest)+
+      '\n| mortgage/rent | $'+format(rent, '9,.2f')+ ' |  '+format((rent/salary)*100, '4,.1f')+'% | '+'#'*int((rent/salary)*100)+
+      '\n|         bills | $'+format(bills, '9,.2f')+ ' |  '+format((bills/salary)*100, '4,.1f')+'% | '+'#'*int((bills/salary)*100)+
+      '\n|          food | $'+format(food, '9,.2f')+ ' |  '+format((food/salary)*100, '4,.1f')+'% | '+'#'*int((food/salary)*100)+
+      '\n|        travel | $'+format(travel, '9,.2f')+ ' |  '+format((travel/salary)*100, '4,.1f')+'% | '+'#'*int((travel/salary)*100)+
+      '\n|           tax | $'+format(tax, '9,.2f')+ ' |  '+format((tax/salary)*100, '4,.1f')+'% | '+'#'*int((tax/salary)*100)+
+      '\n|         extra | $'+format(extra, '9,.2f')+ ' |  '+format((extra/salary)*100, '4,.1f')+'% | '+'#'*int((extra/salary)*100)+
+      '\n'+'------------------------------------------'+'-'*(greatest))
 
 if tax > 75000:
   print(">>> TAX LIMIT REACHED <<<")
---
+++ preps/prep8.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a positive number:\n"))
+
+print()
+i = 0
+while i <= number:
+  print(i)
+  i += 1
