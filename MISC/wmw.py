ccb0f7eedc0d818631cdf1585f95c197f6bc3a2b
TomG22 <giallanzatom@gmail.com>
2022-09-25 14:46:03 -0700

    stock_plotter 2.0

--- MISC/wmw.py
+++ MISC/wmw.py
@@ -1,63 +1,49 @@
-for i in (1,2):
+def wmw():
+  print('[x2:]')
   print('Now watch me whip')
   print('Watch me nae nae')
   print('Now watch me whip whip')
   print('Watch me nae nae\n')
- 
-for i in (1,2): 
+
+def watch():
+  print('[x2:]')
   print('Ooh watch me, watch me')
   print('Ooh watch me, watch me')
   print('Ooh watch me, watch me')
   print('Ooh ooh ooh ooh\n')
-  
-print('Now watch me you')
-print('Now watch me superman')
-print('Now watch me you')
-print('Now watch me superman\n')
 
-for i in (1,2):  
+def wmw_and_watch():
+  wmw()
+  watch()
+
+def s_leg():  
+  print('[x2:]')
   print('Do the stanky leg')
   print('Do the stanky leg\n')
 
-for i in (1,2):   
-  print('Now watch me')
-  print('Bop bop bop bop bop bop bop bop bop')
+def superman(): 
+  print('Now watch me you')
+  print('Now watch me superman')
+  print('Now watch me you')
+  print('Now watch me superman\n')
 
-for i in (1,2):   
-  print('Now watch me whip')
-  print('Watch me nae nae')
-  print('Now watch me whip whip')
-  print('Watch me nae nae\n')
-
-for i in (1,2): 
-  print('Ooh watch me, watch me')
-  print('Ooh watch me, watch me')
-  print('Ooh watch me, watch me')
-  print('Ooh ooh ooh ooh\n')
-  
-print('Now watch me you')
-print('Now watch me superman')
-print('Now watch me you')
-print('Now watch me superman\n')
-
-for i in (1,4):   
+def b_legs():   
+  print('[x4:]')
   print('Now break your legs')
   print('Break your legs\n')
 
-for i in (1,2):
+def bop():
+  print('[x2:]')
   print('Now watch me')
   print('Bop bop bop bop bop bop bop bop bop\n')
-  
-for i in (1,2):   
-  print('Now watch me whip')
-  print('Watch me nae nae')
-  print('Now watch me whip whip')
-  print('Watch me nae nae\n')
 
-for i in (1,2):   
-  print('Ooh watch me, watch me')
-  print('Ooh watch me, watch me')
-  print('Ooh watch me, watch me')
-  print('Ooh ooh ooh ooh\n')
-  
+wmw_and_watch()
+superman()
+s_leg()
+bop()
+wmw_and_watch()
+superman()
+b_legs()
+bop()
+wmw_and_watch()
 print('Crank dat')  
--- PAs/stock_plotter.py
+++ PAs/stock_plotter.py
@@ -1,29 +1,52 @@
-mode = input("Enter mode (horizontal or vertical):\n")
-string = input("Enter a stock input string:\n")
+#mode = input("Enter mode (horizontal or vertical):\n")
+#string = input("Enter a stock input string:\n")
 
-
-i = 0  
-sign = 1
+#Test parameters
+mode = "horizontal"
+string = "u0d1d2u0u1"
 width = 0
 height = 0
+last_num = None
+last_letter = None
 
-while i < len(string):
-  if string[i] != "u" and string[i] != "d":
-
-  height = i+height
-
-while i < len(string):
-  character = string[i]
-  if character == "u":
+def vertical_plots():
+  i = 0
+  width = 19
+  last_num = 0
+  print("#" * width)
+  while i < len(string):
     sign = 1
-  elif character == "d":
-    sign = -1
-  else:
-    character = character*sign
+    character = string[i]
+    if character == "d":
+      sign = -1
+    elif character != "u":
+      character = int(character)# not sure if where to put sign here VV
+      print("#" + " " * (((width // 2 ) + character * sign)) + "*" + " " * (((width // 2) - 2 - character) * sign) + "#")
+      last_num = character + last_num
+    i += 1
+  print("#" * width)
 
-  if mode == "vertical":
-    print("#"+" "*i+" "*(width-i)+"#")
-  elif mode == "horizontal":
-    print("#"+" "*i+" "*(width-i)+"#")
+def horizontal_plots():
+  i = 0
+  width = len(string)
+  height = 19
+  last_num = 0
 
-  i += 1
+  print("#" * width)
+  while i < height:
+    sign = 1
+    character = string[i]
+    if character == "d":
+      sign = -1
+    elif character != "u":
+      if character
+      character = int(character)
+      print("#" + " " * ((width // 2) - character * sign) + "*" + " " * ((width // 2) - character * sign) + "#")
+      last_num = character + last_num
+    i += 1
+  print("#" * width)
+
+if mode == "vertical":
+  vertical_plots()
+elif mode == "horizontal":
+  horizontal_plots() 
